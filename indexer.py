ad51ecba91ca7c91cac2df62460df36a9da76ec7
estingafunner <38468418+estingafunner@users.noreply.github.com>
2022-07-18 04:51:31 -0700

    formatting changes
    Check ## NEED to add...

--- indexer.py
+++ indexer.py
@@ -3,24 +3,26 @@ import tkinter as tk
 import tkinter.filedialog as fdi
 from tkinter import filedialog
 
+### NEED to add a 'save as' when complete, then clear indexOutput.txt
+
 
 def list_files():
     root = tk.Tk()
     startpath = filedialog.askdirectory()
     print(startpath)
+    file_object = open('indexOutput.txt', 'a')
 
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
         indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
+        #print('{}{}/'.format(indent, os.path.basename(root)))
+        file_object.write('{}{}/'.format(indent, os.path.basename(root)) + "\n")
         subindent = ' ' * 4 * (level + 1)
         
-        
         for f in files:
-            file_object = open('indexOutput.txt', 'a')
             #file_object.write("Is it working?")
             file_object.write(('{}{}'.format(subindent, f)) + "\n")
             #print('{}{}'.format(subindent, f))
-            file_object.close()
+    file_object.close()
 
 list_files()
